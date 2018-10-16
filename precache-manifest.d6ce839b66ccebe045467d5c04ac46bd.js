3362713934c971c24336d09ba82e54365d230fbc
Shekhar002 <tshekhar57@gmail.com>
2018-10-16 11:16:31 +0530

    Updates

---
+++ precache-manifest.d6ce839b66ccebe045467d5c04ac46bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65cc5c3bb2be5f34f2fb",
+    "url": "/ReactOnlineshop/static/js/runtime~main.65cc5c3b.js"
+  },
+  {
+    "revision": "dca8453164eea637c58a",
+    "url": "/ReactOnlineshop/static/js/main.dca84531.chunk.js"
+  },
+  {
+    "revision": "816f2ae1629d28032a8c",
+    "url": "/ReactOnlineshop/static/js/1.816f2ae1.chunk.js"
+  },
+  {
+    "revision": "dca8453164eea637c58a",
+    "url": "/ReactOnlineshop/static/css/main.0146989e.chunk.css"
+  },
+  {
+    "revision": "816f2ae1629d28032a8c",
+    "url": "/ReactOnlineshop/static/css/1.0a20b744.chunk.css"
+  },
+  {
+    "revision": "37ef7422fb8eaa9c13b730db6adb7f68",
+    "url": "/ReactOnlineshop/index.html"
+  }
+];
